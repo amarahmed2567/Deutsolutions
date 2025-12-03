fd61089263a63876a85b076106f8826315a43dd1
amarahmed2567 <Amar Ahmed>
2025-12-04 00:25:42 +0200

    Mobile Style eitd

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -80,7 +80,7 @@ const HomePage = () => {
       />
       <Home />
       <motion.div {...sectionAnim}><CardsSlider/></motion.div>
-      <motion.div><ProductsSliderHome /></motion.div>
+      <motion.div {...sectionAnim}><ProductsSliderHome /></motion.div>
       <motion.div {...sectionAnim}><About/></motion.div>
       <motion.div {...sectionAnim}><Services /></motion.div>
       <motion.div {...sectionAnim}><StartNowSlider/></motion.div>
