d1c60b58b6c390da1ad7484e27748d9a322d8df3
amarahmed2567 <Amar Ahmed>
2025-12-07 17:07:01 +0200

    upadet

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import NetworkSphere from "./NetworkSphere"
-const {HeroSection,HeroContent,HeroContentText,MinVarText,Subtext,StatsRow,StatsNumber,StatsNumberText,HeroImage,HerImg,VantaBackground} = style
+const {HeroSection,HeroContent,HeroContentText,MinVarText,Subtext,StatsRow,StatsNumber,StatsNumberText,HeroImage,HerImg,VantaBackground,HeroContainer} = style
 const Home = () => {
   const { t } = useTranslation();
   const vantaRef = useRef(null);
@@ -70,39 +70,41 @@ const Home = () => {
           transition={{ duration: 0.7, ease: "easeOut" }}
         >
         <div ref={vantaRef} className={VantaBackground}></div>
-        <div className={HeroContent}>
-          <div className={HeroContentText}> 
-            <h1>
-              {t('home.hero.title')} <br />
-            <span className={MinVarText}>{t('home.hero.min-var-text')} &nbsp;<AnimatedText/></span>
-            </h1>
+        <div className={HeroContainer}>
+          <div className={HeroContent}>
+            <div className={HeroContentText}> 
+              <h1>
+                {t('home.hero.title')} <br />
+              <span className={MinVarText}>{t('home.hero.min-var-text')} &nbsp;<AnimatedText/></span>
+              </h1>
+            </div>
+            <p className={Subtext}>
+              {t('home.hero.description')}
+            </p>
+            <Link to={'/sign-up'} className="btn-larg">{t('home.hero.cta')}</Link>
+            <div className={StatsRow}>
+              <div>
+                <span className={StatsNumber}>100+</span><br /> <span className={StatsNumberText}>{t('home.stats.clients')}</span>
+              </div>
+              <div>
+                <span className={StatsNumber}>500+</span><br /> <span className={StatsNumberText}>{t('home.stats.translations')}</span> 
+              </div>
+              <div>
+                <span className={StatsNumber}>7+</span><br /> <span className={StatsNumberText}>{t('home.stats.countries')}</span> 
+              </div>
+            </div>
           </div>
-          <p className={Subtext}>
-            {t('home.hero.description')}
-          </p>
-          <Link to={'/sign-up'} className="btn-larg">{t('home.hero.cta')}</Link>
-          <div className={StatsRow}>
-            <div>
-              <span className={StatsNumber}>100+</span><br /> <span className={StatsNumberText}>{t('home.stats.clients')}</span>
-            </div>
-            <div>
-              <span className={StatsNumber}>500+</span><br /> <span className={StatsNumberText}>{t('home.stats.translations')}</span> 
-            </div>
-            <div>
-              <span className={StatsNumber}>7+</span><br /> <span className={StatsNumberText}>{t('home.stats.countries')}</span> 
-            </div>
-          </div>
-        </div>
-        <div className={HeroImage}>
-        {/* <DotLottieReact
-         className={HerImg}
-          src="https://lottie.host/dc07ab64-cce3-4fba-8d5f-8f87d218c1db/A0tSAg3i4V.lottie"
-          // https://lottie.host/f57517fb-64dd-431b-b9a3-4a80d070090e/ML7CF5MMxU.lottie
-          loop
-          autoplay
-        /> */}
+          <div className={HeroImage}>
+          {/* <DotLottieReact
+           className={HerImg}
+            src="https://lottie.host/dc07ab64-cce3-4fba-8d5f-8f87d218c1db/A0tSAg3i4V.lottie"
+            // https://lottie.host/f57517fb-64dd-431b-b9a3-4a80d070090e/ML7CF5MMxU.lottie
+            loop
+            autoplay
+          /> */}
 
-         {/* <NetworkSphere /> */}
+           {/* <NetworkSphere /> */}
+          </div>
         </div>
       </motion.section>
     )
